bc66e09476337ffff4522af5eedf8053c32accf7
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-16 05:36:06 +0800

    feat: 設定全域 title

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <Navbar />
+    <!-- 汽車單頁介紹 -->
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default {
     preset: 'vercel-edge',
   },
   modules: ['@nuxtjs/tailwindcss'],
+  head: {
+    title: 'Car Trader',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 };
