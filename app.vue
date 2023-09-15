a3bca7fc58ff8e6120283c0e5c6ddd2143f28f10
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-16 04:19:20 +0800

    feat: 完成 Home Banner 元件

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
     <Navbar />
-    <section
-      class="
-        relative
-        h-screen
-        w-100
-        bg-no-repeat bg-cover bg-bottom
-        bg[url('@/assets/home-hero-banner.webp')]
-      "
-    ></section>
   </div>
 </template>
