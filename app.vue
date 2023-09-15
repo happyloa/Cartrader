af5949056ab610e7156008c7617aa3c791587ad0
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-16 03:43:54 +0800

    1 binaries uploaded

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <Navbar />
-    
+    <section
+      class="
+        relative
+        h-screen
+        w-100
+        bg-no-repeat bg-cover bg-bottom
+        bg[url('')]
+      "
+    ></section>
   </div>
 </template>
