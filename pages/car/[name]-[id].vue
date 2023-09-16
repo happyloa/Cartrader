0fe04a702b613517f2a46798a15604fc6ff86396
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-17 05:24:53 +0800

    feat: 新增註解

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,14 +1,18 @@
 <template>
+  <!-- 單一車輛頁面的容器 -->
   <div><CarSinglePage /></div>
 </template>
 
 <script setup>
+// 獲取當前路由資訊
 const route = useRoute();
 
+// 設定網頁標題，標題會根據路由參數的名稱轉換為標題格式
 useHead({
   title: toTitleCase(route.params.name),
 });
 
+// 函式：將字串轉換為標題格式（首字母大寫，其餘小寫）
 function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 包含車輛列表和內容的容器區域 -->
   <section
     class="
       mx-auto
@@ -14,22 +15,26 @@
     "
   >
     <div class="mt-32 flex">
-      <!-- 列表頁面側邊攔 -->
+      <!-- 車輛列表頁面的側邊欄 -->
       <CarsSideBar />
+      <!-- Nuxt頁面，用於顯示車輛資訊 -->
       <NuxtPage />
     </div>
   </section>
 </template>
 
 <script setup>
+// 獲取當前路由資訊
 const route = useRoute();
 
+// 設定網頁標題，標題會包含城市和製造商（如果有的話）
 useHead({
   title: `${
     route.params.make ? toTitleCase(route.params.make) : 'Cars'
   } in ${toTitleCase(route.params.city)}`,
 });
 
+// 函式：將字串轉換為標題格式（首字母大寫，其餘小寫）
 function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
