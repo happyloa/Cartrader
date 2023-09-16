d7f3456141c383c590f4707f55e17cbdebe1d4a1
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-17 06:03:12 +0800

    feat: 新增錯誤訊息頁面

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="h-screen flex flex-col justify-center items-center">
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl">{{ error.message }}</p>
+    <NuxtLink
+      to="/"
+      class="border rounded-full mt-7 text-2xl bg-blue-400 px-7 py-3 text-white"
+      >Go back</NuxtLink
+    >
+  </main>
+</template>
+
+<script setup>
+const error = useError();
+</script>
