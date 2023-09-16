c597cd743a5cb703f1e91d39f4f7a9ec61e508bf
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-17 04:37:07 +0800

    refactor: 讓 props 變數縮短一點

--- components/Home/HomeHeading.vue
+++ components/Home/HomeHeading.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- 使用props來顯示標題文字 -->
-  <h1 class="text-8xl font-serif mb-10 text-white">{{ HomeBannerHeading }}</h1>
+  <h1 class="text-8xl font-serif mb-10 text-white">{{ Heading }}</h1>
 </template>
 
 <script>
 export default {
   props: {
-    HomeBannerHeading: String, // 定義一個名為 HomeBannerHeading 的 props，類型為 String
+    Heading: String, // 定義一個名為 Heading 的 props，類型為 String
   },
 };
 </script>
