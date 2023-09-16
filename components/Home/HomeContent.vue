46956934fde7656c20085fcc82b80068059c9597
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-17 04:16:02 +0800

    remove: 移除多餘的大於符號

--- components/Home/HomeContent.vue
+++ components/Home/HomeContent.vue
@@ -3,7 +3,7 @@
   <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
   <main class="z-10 absolute text-center w-1/2">
     <!-- 標題文字 -->
-    <HomeHeading HomeBannerHeading="Find Your Drive" />>
+    <HomeHeading HomeBannerHeading="Find Your Drive" />
     <!-- 搜尋列 -->
     <HomeSearch />
   </main>
