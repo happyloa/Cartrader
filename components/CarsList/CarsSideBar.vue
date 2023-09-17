2cee7559e2767f906c5b270f34c9d52d699deb55
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-18 02:48:26 +0800

    feat: 讓側邊攔有搜尋城市的功能

--- components/CarsList/CarsSideBar.vue
+++ components/CarsList/CarsSideBar.vue
@@ -6,16 +6,31 @@
       <!-- 地點過濾標題 -->
       <h3>Location</h3>
       <!-- 當前選擇的地點 -->
-      <h3 class="text-blue-400 capitalize">Toronto</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('location')">
+        {{ route.params.city }}
+      </h3>
       <!-- 注釋掉的部分是彈出搜尋窗，用於選擇其他地點 -->
-      <!-- 
-      <div class="absolute border shadow left-56 top-1 p-5 -m-1 bg-white">
-        <input type="text" class="border p-1 rounded" />
-        <button class="bg-sky-400 w-full mt-2 rounded text-white p-1 hover:bg-blue-400">
+
+      <div
+        class="absolute border shadow left-48 top-1 p-5 -m-1 bg-white"
+        v-if="modal.location"
+      >
+        <input type="text" class="border p-1 rounded" v-model="city" />
+        <button
+          class="
+            bg-sky-400
+            w-full
+            mt-2
+            rounded
+            text-white
+            p-1
+            hover:bg-blue-400
+          "
+          @click="onChangeLocation"
+        >
           Apply
         </button>
       </div>
-      -->
     </div>
 
     <!-- 製造商過濾區塊 -->
@@ -35,3 +50,25 @@
     </div>
   </aside>
 </template>
+
+<script setup>
+const city = ref('');
+const route = useRoute();
+
+const modal = ref({
+  location: false,
+  make: false,
+  price: false,
+});
+
+const updateModal = (key) => {
+  modal.value[key] = !modal.value[key];
+};
+
+const onChangeLocation = () => {
+  if (!city.value) return;
+  updateModal('location');
+  navigateTo(`/city/${city.value}/car/${route.params.make}`);
+  city.value = '';
+};
+</script>
