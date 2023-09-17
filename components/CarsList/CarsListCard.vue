77df2ab8bc73646f9bd36834e0a40ed1b2a2a401
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-17 22:02:19 +0800

    feat: 新增註解

--- components/CarsList/CarsListCard.vue
+++ components/CarsList/CarsListCard.vue
@@ -1,30 +1,37 @@
 <template>
   <!-- 汽車卡片列表 -->
   <main class="w-full">
-    <div
+    <!-- 迭代顯示每輛汽車的卡片 -->
+    <section
       class="shadow border overflow-hidden mb-5 cursor-pointer h-[200px]"
       v-for="car in cars"
       :key="car.id"
       :car="car"
       @click="navigateTo(`/car/${car.name.replace(/\s/g, '')}-${car.id}`)"
     >
-      <div class="flex h-full">
+      <article class="flex h-full">
+        <!-- 顯示汽車圖片 -->
         <img :src="car.url" alt="" class="w-[300px] h-full" />
         <div class="p-4 flex flex-col">
+          <!-- 顯示汽車名稱 -->
           <h2 class="text-2xl text-blue-700">{{ car.name }}</h2>
+          <!-- 顯示汽車描述 -->
           <p class="text-gray-700">
             {{ car.description }}
           </p>
+          <!-- 顯示汽車價格 -->
           <h2 class="mt-auto text-xl">${{ car.price }}</h2>
         </div>
-      </div>
-    </div>
+      </article>
+    </section>
   </main>
 </template>
 
 <script setup>
+// 從 useCars() 鉤子中獲取汽車數據
 const { cars } = useCars();
 
+// 定義 props，接收一個 car 物件作為參數
 const props = defineProps({
   car: Object,
 });
--- composables/useCars.js
+++ composables/useCars.js
@@ -1,7 +1,10 @@
+// 引入汽車數據，假設它們存儲在 '@/data/cars.json'
 import cars from '@/data/cars.json';
 
+// 定義一個自定義 hook，用於獲取汽車數據
 export const useCars = () => {
+  // 返回包含汽車數據的物件
   return {
-    cars,
+    cars, // 返回汽車數據
   };
 };
