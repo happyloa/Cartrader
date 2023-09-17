844a2983cf6bb33f20653b2dda5c52d5d1f96d77
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-18 01:39:31 +0800

    feat: 動態抓取汽車單頁資訊

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,17 +1,26 @@
 <template>
-  <!-- 單一車輛頁面的容器 -->
-  <CarSinglePage />
+  <div v-if="car">
+    <!-- 單一車輛頁面的容器 -->
+    <CarSinglePage :car="car" />
+  </div>
 </template>
 
 <script setup>
 // 獲取當前路由資訊
 const route = useRoute();
+const { cars } = useCars();
 // 引入 toTitleCase function
 const { toTitleCase } = useUtilities();
 
 // 設定網頁標題，標題會根據路由參數的名稱轉換為標題格式
 useHead({
   title: toTitleCase(route.params.name),
+});
+
+const car = computed(() => {
+  return cars.find((c) => {
+    return c.id === parseInt(route.params.id);
+  });
 });
 
 // 使用 custom.vue 作為 layout
