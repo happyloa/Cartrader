f27923d9e6548b53b9ce72e3b7e6b0d1cca7b6ad
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-17 21:48:41 +0800

    feat: 新增汽車資料並動態抓取

--- components/CarsList/CarsListCard.vue
+++ components/CarsList/CarsListCard.vue
@@ -1,24 +1,31 @@
 <template>
   <!-- 汽車卡片列表 -->
   <main class="w-full">
-    <!-- 汽車卡片1 -->
     <div
-      class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+      class="shadow border overflow-hidden mb-5 cursor-pointer h-[200px]"
+      v-for="car in cars"
+      :key="car.id"
+      :car="car"
+      @click="navigateTo(`/car/${car.name.replace(/\s/g, '')}-${car.id}`)"
     >
       <div class="flex h-full">
-        <img
-          src="https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg?w=2000"
-          alt=""
-          class="w-[300px] h-full"
-        />
+        <img :src="car.url" alt="" class="w-[300px] h-full" />
         <div class="p-4 flex flex-col">
-          <h2 class="text-2xl text-blue-700">Volvo XC40</h2>
+          <h2 class="text-2xl text-blue-700">{{ car.name }}</h2>
           <p class="text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            {{ car.description }}
           </p>
-          <h2 class="mt-auto text-xl">$39,555</h2>
+          <h2 class="mt-auto text-xl">${{ car.price }}</h2>
         </div>
       </div>
     </div>
   </main>
 </template>
+
+<script setup>
+const { cars } = useCars();
+
+const props = defineProps({
+  car: Object,
+});
+</script>
