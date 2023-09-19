c5cf2a974ab1d85604f947bea0c1171ddb897276
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-20 01:53:12 +0800

    comment: 加上註解

--- components/CarsList/Card.vue
+++ components/CarsList/Card.vue
@@ -1,36 +1,46 @@
 <script setup>
-import Filled from '@/assets/heartFilled.webp';
-import Outline from '@/assets/heartOutline.webp';
+  // 引入兩種不同的心形圖標：一個填充（已收藏），一個空心（未收藏）
+  import Filled from '@/assets/heartFilled.webp';
+  import Outline from '@/assets/heartOutline.webp';
 
-const props = defineProps({
-  car: Object,
-});
+  // 定義元件屬性。這個元件預期接收一個名為 "car" 的 Object。
+  const props = defineProps({
+    car: Object,
+  });
 
-const favored = useState(`favored-${props.car.id}`, () => {
-  return false;
-});
+  // 使用 useState 創建一個反應性的狀態變量 "favored" 用來記錄是否已經收藏了這輛車。
+  // 我們使用車輛的 ID 來創建一個獨一無二的狀態鍵。
+  // 初始值設為 false，表示未收藏。
+  const favored = useState(`favored-${props.car.id}`, () => {
+    return false;
+  });
 </script>
 
 <template>
+  <!-- 整個卡片的容器，設定了一些風格和高度 -->
   <section
     class="relative shadow border overflow-hidden mb-7 cursor-pointer h-[200px]"
   >
-    <!-- 使用 v-for 迴圈遍歷 cars 陣列，為每輛汽車生成一個卡片 -->
+    <!-- 收藏按鈕，使用心形圖標 -->
+    <!-- 根據 "favored" 狀態變量顯示不同的圖標 -->
+    <!-- @click 事件更改 "favored" 的狀態 -->
     <img
       :src="favored ? Filled : Outline"
-      alt="蒐藏圖示"
+      alt="收藏圖示"
       class="absolute w-7 right-5 top-4 z-20"
       @click="favored = !favored"
     />
-    <!-- 單一汽車卡片容器 -->
+
+    <!-- 單一汽車的主要信息區塊 -->
     <article
       class="flex h-full"
       @click="navigateTo(`/car/${car.name}-${car.id}`)"
     >
-      <!-- 汽車圖片區塊：使用綁定的 car.url 作為圖片來源 -->
+      <!-- 汽車圖片，使用 NuxtImg 元件來優化圖片加載 -->
+      <!-- :src 綁定到車輛數據的 url 屬性 -->
       <NuxtImg :src="car.url" alt="" class="w-[300px]" />
 
-      <!-- 汽車資訊區塊：包括名稱、描述和價格 -->
+      <!-- 汽車的主要資訊區塊 -->
       <div class="p-4 pr-9 flex flex-col">
         <!-- 汽車名稱 -->
         <h2 class="text-2xl text-blue-700">{{ car.name }}</h2>
@@ -38,7 +48,8 @@ const favored = useState(`favored-${props.car.id}`, () => {
         <p class="text-gray-700">
           {{ car.description }}
         </p>
-        <!-- 汽車價格：自動貼齊到底部 (mt-auto) -->
+        <!-- 汽車價格 -->
+        <!-- 使用 mt-auto 來將價格自動貼齊到底部 -->
         <span class="mt-auto text-xl">${{ car.price }}</span>
       </div>
     </article>
--- components/CarsList/Cards.vue
+++ components/CarsList/Cards.vue
@@ -1,10 +1,15 @@
 <script setup>
-// 從 useCars() 自定義鉤子中獲取所有的汽車數據
+// 引入 useCars 自定義鉤子。這個鉤子專門用來獲取車輛相關數據。
 const { cars } = useCars();
+// `cars` 是一個反應性的數據，它會包含所有的汽車數據。
 </script>
 
 <template>
+  <!-- 主要的 HTML 結構，包含一個 <main> 標籤，用來封裝車輛列表 -->
   <main>
+    <!-- 使用 v-for 循環遍歷 cars 數據，為每一輛車創建一個 CarsListCard 元件 -->
+    <!-- :key 是為了提高 Vue 的性能，每個元件都應該有一個唯一的 key -->
+    <!-- :car="car" 將當前循環中的車輛數據傳遞給 CarsListCard 元件 -->
     <CarsListCard v-for="car in cars" :key="car.id" :car="car" />
   </main>
 </template>
