9c357a341d404400ad1d33e3663e72f942f4e2ed
happyloa <83556435+happyloa@users.noreply.github.com>
2023-09-25 05:15:50 +0800

    refactor: 改用 setup 的方式來定義 props

--- components/Home/HomeHeading.vue
+++ components/Home/HomeHeading.vue
@@ -1,12 +1,13 @@
 <template>
-  <!-- 使用props來顯示標題文字 -->
+  <!-- 這是一個簡單的標題組件。它將使用從外部傳入的 'Heading' 屬性作為其內容。 -->
+  <!-- 樣式被設置為 text-8xl, font-serif, mb-10, 和 text-white。 -->
   <h1 class="text-8xl font-serif mb-10 text-white">{{ Heading }}</h1>
 </template>
 
-<script>
-export default {
-  props: {
-    Heading: String, // 定義一個名為 Heading 的 props，類型為 String
-  },
-};
+<script setup>
+// 這裡定義了組件的 props。
+// 我們期望從外部接收一個名為 'Heading' 的屬性，其類型為 String。
+defineProps({
+  Heading: String,
+});
 </script>
